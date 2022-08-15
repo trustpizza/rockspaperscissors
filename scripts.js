d696cee03e16a88b5e7578924188fe2cb5d35723
trustpizza <maxelolsson@gmail.com>
2022-08-15 18:18:04 -0500

    Add replayability and best of 5 functionality

--- scripts.js
+++ scripts.js
@@ -1,29 +1,125 @@
-const container = document.querySelector('#container');
+let playerScore = 0;
+let computerScore = 0;  
+let roundResult;
 
-const buttons = document.createElement('div');
+const container = document.querySelector('#container'); 
+
+const buttons = document.createElement('div'); // Populate buttons
 buttons.classList.add('content');
-buttons.textContent = 'Select:   ';
 
-container.appendChild(buttons)
+container.appendChild(buttons);
 
-const rock = document.createElement('button');
+const rock = document.createElement('button'); // Rock
+rock.classList.add('button');
 rock.textContent = 'rock';
 rock.addEventListener('click', () => {
-    console.log("rock")
+    playRound('rock', computerPlay());
 })
 buttons.appendChild(rock);
 
 
-const paper = document.createElement('button');
+const paper = document.createElement('button'); // Paper
+paper.classList.add('button')
 paper.textContent = 'paper';
 paper.addEventListener('click', () => {
-    console.log("paper")
+    playRound('paper', computerPlay())
 })
 buttons.appendChild(paper);
 
-const scissors = document.createElement('button');
+const scissors = document.createElement('button'); // Scissors
+scissors.classList.add('button');
 scissors.textContent = 'scissors';
 scissors.addEventListener('click', () => {
-    console.log('scissors')
+    playRound('scissors', computerPlay());
 })
 buttons.appendChild(scissors)
+
+function computerPlay() { // Computer random choice
+    let randomInt = (Math.floor(Math.random() * 3) +1);
+    if(randomInt === 1) {
+        return("rock");
+    } else if (randomInt === 2) {
+        return("paper");
+    } else if (randomInt === 3) {
+        return("scissors");
+    } 
+}
+
+const scoreboard = document.createElement('div')
+scoreboard.classList.add('score')
+container.appendChild(scoreboard);
+
+const playerScoreBoard = document.createElement('div'); // Creates a scoreboard for the player
+playerScoreBoard.classList.add('scoreboard');
+document.addEventListener('click', () => {
+    playerScoreBoard.innerHTML = "<p> Your Score is " + playerScore + "</p>";
+
+})
+scoreboard.appendChild(playerScoreBoard);
+
+const computerScoreBoard = document.createElement('div'); // Creates computer scoreboard
+computerScoreBoard.classList.add('scoreboard');
+document.addEventListener('click', () => {
+    computerScoreBoard.innerHTML = "<p> Computer's Score is " + computerScore + "</p>";
+
+})
+scoreboard.appendChild(computerScoreBoard);
+
+const results = document.createElement('div'); // Alert results Div
+results.classList.add('results');
+document.addEventListener('click', () => {
+    if (roundResult == undefined) {
+        results.innerHTML = "";
+    } else{ 
+    results.innerHTML = "<p>" + roundResult + "</p>";
+    }
+});
+container.appendChild(results);
+
+
+function playRound(playerSelection, computerSelection) { // Play a round of Rocks Paper Scissors
+    if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+    roundResult = `You win! ${playerSelection} beats ${computerSelection}!`;
+    playerScore++;
+    gameEnd();
+} else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+    roundResult = `You win! ${playerSelection} beats ${computerSelection}!`;
+    playerScore++;
+    gameEnd();
+} else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+    roundResult = `You win! ${playerSelection} beats ${computerSelection}!`;
+    playerScore++;
+    gameEnd();
+} else if ((playerSelection === "rock") && (computerSelection === "paper")) {
+    roundResult = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    computerScore++;
+    gameEnd();
+} else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
+    roundResult = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    computerScore++;
+    gameEnd();
+} else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
+    roundResult = `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    computerScore++; 
+    gameEnd();
+} else if (playerSelection === computerSelection) {
+    roundResult = "It's a Tie!";
+} 
+}
+
+
+function gameEnd() {
+    if (playerScore === 5) {
+        let replayMessage = confirm("You Win! Would you like to play again?");
+        if (replayMessage) {
+            playerScore = 0;
+            computerScore = 0;
+        } 
+    } else if (computerScore === 5) {
+        let replayMessage = confirm("You Lose! Would you like to play again?");
+        if (replayMessage) {
+            playerScore = 0;
+            computerScore = 0;
+        }
+    } 
+}
